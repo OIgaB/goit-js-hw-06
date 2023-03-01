3b15c9b2748b08b2e55ee769c8e9595ef50fea47
Olga Bagrii <linkbolya@gmail.com>
2023-03-01 05:38:26 +0200

    Update 5th task

--- js/task-05.js
+++ js/task-05.js
@@ -5,8 +5,7 @@ const titleNameEl = document.querySelector('#name-output');
 
 inputEl.addEventListener('input', (event) => {
     titleNameEl.textContent = event.target.value;
-    // Працює і без цієї умови
-    // if (event.target.value === '') {
-    //     titleNameEl.textContent = 'Anonymous';
-    // }   
+    if (event.target.value === '') {
+        titleNameEl.textContent = 'Anonymous';
+    }   
 });
